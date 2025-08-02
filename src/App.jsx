1347301943274884dccd2453e2d0ef54a2f0649b
Priyam Aneja <priyamaneja2002@gmail.com>
2025-08-02 20:24:01 +0530

    Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import Model from "./components/Model"
 import Features from "./components/Features"
 import HowItWorks from "./components/HowItWorks"
